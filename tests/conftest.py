935120e04378822e398ebca00761a6f015ece076
Stefan <wan73605@burktom.anonaddy.com>
2026-04-12 23:12:50 +0200

    Update TEST_TOKEN to a placeholder value

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,5 +24,5 @@ sys.modules['homeassistant.helpers.entity_platform'] = MagicMock()
 # Add custom_components to the path so poollab can be imported
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'custom_components'))
 
-TEST_TOKEN = "e3ce8106035d4196066e33f2ef8a313ed3f7f57d836b7e5cca212509dbaccb7fa3c4ca0b8db5905b"
+TEST_TOKEN = "TEST_TOKEN"
 TEST_DEVICE_ID = "device_123"
